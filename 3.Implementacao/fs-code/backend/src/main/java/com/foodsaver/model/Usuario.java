87f1e5900a1e9b5aae52b1582564dd2b2dabd730
Fábio Adriano Silveira <fabioadrianosilveira456@gmail.com>
2026-05-20 17:08:17 -0300

    Implementação inicial do UC - 02

---
+++ 3.Implementacao/fs-code/backend/src/main/java/com/foodsaver/model/Usuario.java
@@ -0,0 +1,19 @@
+package com.foodsaver.model;
+
+public class Usuario {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+
+    public Usuario(Long id, String nome, String email, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getEmail() { return email; }
+    public String getSenha() { return senha; }
+}
