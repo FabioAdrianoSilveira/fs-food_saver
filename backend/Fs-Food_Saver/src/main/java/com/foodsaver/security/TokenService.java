77e4b5ff762f87e7ac9f7ebdf2d90c3f4abe00c2
Ramon_Zo0m <ramoncaruso123@gmail.com>
2026-06-24 12:07:26 -0300

    Segurança

---
+++ backend/Fs-Food_Saver/src/main/java/com/foodsaver/security/TokenService.java
@@ -0,0 +1,50 @@
+package com.foodsaver.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.foodsaver.model.Usuario;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    // Chave secreta (em produção, isto fica nas variáveis de ambiente)
+    private String secret = "123456";
+
+    public String gerarToken(Usuario usuario) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("foodsaver-api")
+                    .withSubject(usuario.getEmail())
+                    .withExpiresAt(gerarDataExpiracao())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Erro ao gerar token jwt", exception);
+        }
+    }
+
+    public String validarToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("foodsaver-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            return "";
+        }
+    }
+
+    private Instant gerarDataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
